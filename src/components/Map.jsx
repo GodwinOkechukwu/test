87677d429360248b786443efcfbee9cb9c15d028
kenesongodwin@gmail.com <kenesongodwin@gmail.com>
2025-04-14 07:11:00 +0100

    secret key

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,7 +1,7 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 
 const Map = ({ lat, lng }) => (
-  <LoadScript googleMapsApiKey="AIzaSyAFnaWen9hCjmtvRubuwA8hpC2JMZEOOuA">
+  <LoadScript googleMapsApiKey={import.meta.env.VITE_GOOGLE_MAPS_API_KEY}>
     <GoogleMap
       mapContainerStyle={{ width: "100%", height: "300px" }}
       center={{ lat, lng }}
